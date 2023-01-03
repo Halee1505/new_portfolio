3be58c83fd3ff275e2bc95cf7e55d60f0a5d3271
LinhNguyen1505 <danhauloc@gmail.com>
2023-01-03 16:24:41 +0700

    reverse

--- controller/image.controller.js
+++ controller/image.controller.js
@@ -2,13 +2,15 @@ const Image = require("../model/image.model.js");
 
 class Img {
   index(req, res, next) {
+    //find and reverse the order of the images
     Image.find({}, function (err, key) {
-      if (!err) res.send(key);
+      if (!err) res.send(key.reverse());
       else {
         res.json("error");
       }
     });
   }
+
   findAlbum(req, res, next) {
     Image.findById(req.params.id, function (err, key) {
       if (!err) res.send(key);
